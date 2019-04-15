5c4964b181542a345c295e15e05c4c4b7a11d097
Tuan Phan <47899656+tuanhus93@users.noreply.github.com>
2019-04-14 23:18:11 -0400

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -81,7 +81,7 @@ def Ichimoku(close, A, B, signal):
             and close[-2] < A[-2] and close[-2] < B[-2] \
             and close[-3] < A[-3] and close[-3] < B[-3] \
             and close[-1] < close[-2] < close[-3] \
-            and close[-1] < signal[3]:
+            and close[-1] < signal[2]:
         return -1
     elif close[-1] > A[-1] and close[-1] > B[-1] \
             and close[-2] > A[-2] and close[-2] > B[-2] \
